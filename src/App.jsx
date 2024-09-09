77ccf1fd510684ce23a2fbaf4f79c8025bbfb529
Alibaba <abdulmanafaliu414@gmail.com>
2024-09-09 16:10:54 +0000

    initia commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <section className='card'>
+        <p>My name is Alibaba</p>
+      </section>
+    </div>
+  )
+}
+
+export default App
